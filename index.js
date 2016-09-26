b63639476c5d1020df05a3e943f2271857075bd9
David Leuenberger <david.leuenberger@hsr.ch>
2016-09-26 18:26:05 +0200

    add js to switch from overview-page to new-note-page and back

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 function changeStyle(event) {
     $("#stylesheetLink").attr("href", event.target.value);
 }
+
+function showNewNotePage() {
+    $(".overview-page").hide();
+    $(".new-note-page").show();
+}
+
+function hideNewNotePage() {
+    $(".overview-page").show();
+    $(".new-note-page").hide();
+}
+
+function cancelNewNotePage() {
+    hideNewNotePage();
+}
+
+function saveNewNotePage() {
+    hideNewNotePage();
+}
