02dda84467eabd883933a7b19b9cce951743b2c6
David Leuenberger <david.leuenberger@hsr.ch>
2016-09-26 19:45:07 +0200

    use handlebar.js to render notes, remove static entries, add daten.js with daten array

---
+++ daten.js
@@ -0,0 +1,6 @@
+var notes = [
+    {"id":0, "due-date":"30.09.20116", "title":"Note1", "description": "Beschreibung1", "importance":3, "finished":false, "finishedAt":""},
+    {"id":1, "due-date":"10.10.20116", "title":"Note2", "description": "Sehr lange Zeile alle am Stück 012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789", "importance":1, "finished":true, "finishedAt":"26.09.2016"},
+    {"id":2, "due-date":"01.10.20116", "title":"Note3", "description": "Beschreibung3\nLine2\nLine3\nLine4\nLine5", "importance":5, "finished":false, "finishedAt":""},
+    {"id":3, "due-date":"09.10.20116", "title":"Note4", "description": "Beschreibung4", "importance":5, "finished":false, "finishedAt":""},
+];
