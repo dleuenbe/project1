ac18a9f7566b36f34045809bbd02b4f9979a9937
David Leuenberger <david.leuenberger@hsr.ch>
2016-09-27 23:44:13 +0200

    change daten.js, add renderNotes function, implement ordering

--- index.js
+++ index.js
@@ -20,6 +20,17 @@ function saveNewNotePage() {
     hideNewNotePage();
 }
 
+function renderNotes() {
+    var renderingNotes = notes.filter(d => $('#show-finished').prop('checked') || !d.finished).sort(compareById);
+    var notesTemplateText = $("#notes-template").html();
+    $(".note-bar").get(0).innerHTML = Handlebars.compile(notesTemplateText)(renderingNotes);
+}
+
+function compareById(n1, n2) {
+    var orderByField = $(".filter-item:checked").prop('id').substr('order-by-'.length);
+    return n2[orderByField] == n1[orderByField] ? 0 : n2[orderByField] < n1[orderByField] ? -1 : 1;
+}
+
 function registerHandlebarIfConf() {
     Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
 
