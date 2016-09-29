bb822fe017f95e4631fb0f09620b4cf48fabbca2
David Leuenberger <david.leuenberger@hsr.ch>
2016-09-29 21:58:30 +0200

    afix wrong dates

--- daten.js
+++ daten.js
@@ -1,6 +1,6 @@
 var defaultNotesFromJS = [
-    {"id":0, "dueDate":"01.09.20116", "title":"Note1", "description": "Beschreibung1", "importance":3, "finished":true, "finishedAt":"31.1.2010"},
-    {"id":1, "dueDate":"10.10.20116", "title":"Note2", "description": "Sehr lange Zeile alle am Stück 012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789", "importance":1, "finished":true, "finishedAt":"26.09.2016"},
-    {"id":2, "dueDate":"03.10.20116", "title":"Note3", "description": "Beschreibung3\nLine2\nLine3\nLine4\nLine5", "importance":5, "finished":false, "finishedAt":""},
-    {"id":3, "dueDate":"09.10.20116", "title":"Note4", "description": "Beschreibung4", "importance":4, "finished":false, "finishedAt":""},
+    {"id":0, "dueDate":"01.09.2016", "title":"Note1", "description": "Beschreibung1", "importance":3, "finished":true, "finishedAt":"31.1.2010"},
+    {"id":1, "dueDate":"10.10.2016", "title":"Note2", "description": "Sehr lange Zeile alle am Stück 012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789", "importance":1, "finished":true, "finishedAt":"26.09.2016"},
+    {"id":2, "dueDate":"03.10.2016", "title":"Note3", "description": "Beschreibung3\nLine2\nLine3\nLine4\nLine5", "importance":5, "finished":false, "finishedAt":""},
+    {"id":3, "dueDate":"09.10.2016", "title":"Note4", "description": "Beschreibung4", "importance":4, "finished":false, "finishedAt":""},
 ];
