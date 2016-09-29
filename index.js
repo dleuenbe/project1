e3bbba444c3ec9b8cbac0de8d2dbb132921eb4c0
David Leuenberger <david.leuenberger@hsr.ch>
2016-09-29 22:01:07 +0200

    remove unused switch statement

--- index.js
+++ index.js
@@ -61,26 +61,8 @@ function saveToLocalStorage() {
 
 function registerHandlebarIfConf() {
     Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
-
-        switch (operator) {
-            case '==':
-                return (v1 == v2) ? options.fn(this) : options.inverse(this);
-            case '===':
-                return (v1 === v2) ? options.fn(this) : options.inverse(this);
-            case '<':
-                return (v1 < v2) ? options.fn(this) : options.inverse(this);
-            case '<=':
-                return (v1 <= v2) ? options.fn(this) : options.inverse(this);
-            case '>':
-                return (v1 > v2) ? options.fn(this) : options.inverse(this);
-            case '>=':
-                return (v1 >= v2) ? options.fn(this) : options.inverse(this);
-            case '&&':
-                return (v1 && v2) ? options.fn(this) : options.inverse(this);
-            case '||':
-                return (v1 || v2) ? options.fn(this) : options.inverse(this);
-            default:
-                return options.inverse(this);
+        if (operator === '<=') {
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
         }
     });
 }
