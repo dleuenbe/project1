4b17b3a61b955ce39b43418f9a795e92b5e96561
David Leuenberger <david.leuenberger@hsr.ch>
2016-10-08 12:15:56 +0200

    use namespace

--- js/LocalStorageService.js
+++ js/LocalStorageService.js
@@ -1,44 +1,48 @@
 /**
  * Created by david on 07.10.16.
  */
-var localStorageService = (function () {
+;(function(namespace) {
+    'use strict';
 
-    function publicSaveEntry(note) {
-        var notes = publicGetAll();
-        privateUpdateNote(notes, note);
-        localStorage.setItem("notes", JSON.stringify(privateRemoveNoteWithoutId(notes)));
-    }
+    namespace.localStorageService = (function () {
 
-    function privateRemoveNoteWithoutId(notes) {
-        return notes.filter(n => !isNaN(n.id));
-    }
+        function publicSaveEntry(note) {
+            var notes = publicGetAll();
+            privateUpdateNote(notes, note);
+            localStorage.setItem("notes", JSON.stringify(privateRemoveNoteWithoutId(notes)));
+        }
 
-    function privateUpdateNote(notes, note) {
-        for (var i = 0; i < notes.length; i++) {
-            if (notes[i].id == note.id) {
-                notes.splice(i, 1, note);
-                return;
+        function privateRemoveNoteWithoutId(notes) {
+            return notes.filter(n => !isNaN(n.id));
+        }
+
+        function privateUpdateNote(notes, note) {
+            for (var i = 0; i < notes.length; i++) {
+                if (notes[i].id == note.id) {
+                    notes.splice(i, 1, note);
+                    return;
+                }
             }
+            notes.push(note);
         }
-        notes.push(note);
-    }
 
-    function publicGetEntry(id) {
-        var notesFromLocalStorage = localStorage.getItem("notes");
-        return notesFromLocalStorage.filter((pos, i) => i.id == id)[0];
-    }
-
-    function publicGetAll() {
-        var notesFromLocalStorage = localStorage.getItem("notes");
-        if (notesFromLocalStorage === null) {
-            return defaultNotesFromJS;
+        function publicGetEntry(id) {
+            var notesFromLocalStorage = localStorage.getItem("notes");
+            return notesFromLocalStorage.filter((pos, i) => i.id == id)[0];
         }
-        return privateRemoveNoteWithoutId(JSON.parse(notesFromLocalStorage));
-    }
 
-    return {
-        save: publicSaveEntry,
-        get: publicGetEntry,
-        getAll: publicGetAll
-    };
-})();
+        function publicGetAll() {
+            var notesFromLocalStorage = localStorage.getItem("notes");
+            if (notesFromLocalStorage === null) {
+                return defaultNotesFromJS;
+            }
+            return privateRemoveNoteWithoutId(JSON.parse(notesFromLocalStorage));
+        }
+
+        return {
+            save: publicSaveEntry,
+            get: publicGetEntry,
+            getAll: publicGetAll
+        };
+    })();
+}) (window.notesAppNamespace = window.notesAppNamespace || {});
--- js/notes.js
+++ js/notes.js
@@ -1,140 +1,141 @@
+;(function(namespace) {
+    'use strict';
 
-'use strict';
+    $(function () {
+        registerHandlebarIfLte();
+        $("header > select").change(changeStyle);
+        $(".new-note").click(newNote);
+        $("#cancel-new-note").click(cancelEdit);
+        $("#save-new-note").click(saveEdit);
+        $("#setFinishedDate").click(setFinishedDate);
+        $("#clearFinishedDate").click(clearFinishedDate);
+        $("#show-finished").change(updateFilter);
+        $(".filter-item").change(updateOrder);
+        $("#show-finished + label, .filter-item + label").mousedown(false);
+        $(".priority-field > label").click(updatePriorityView);
+        $("#errorMessage").hide();
+        updateOrder();
+        updateFilter();
+        renderNotes();
+    });
 
-$(function () {
-    registerHandlebarIfLte();
-    $("header > select").change(changeStyle);
-    $(".new-note").click(newNote);
-    $("#cancel-new-note").click(cancelEdit);
-    $("#save-new-note").click(saveEdit);
-    $("#setFinishedDate").click(setFinishedDate);
-    $("#clearFinishedDate").click(clearFinishedDate);
-    $("#show-finished").change(updateFilter);
-    $(".filter-item").change(updateOrder);
-    $("#show-finished + label, .filter-item + label").mousedown(false);
-    $(".priority-field > label").click(updatePriorityView);
-    $("#errorMessage").hide();
-    updateOrder();
-    updateFilter();
-    renderNotes();
-});
-
-function changeStyle(event) {
-    $("#stylesheetLink").attr("href", event.target.value);
-}
-
-function updateOrder() {
-    var fieldName = $(".filter-item:checked").prop('id').substr('order-by-'.length);
-    notesService.orderByField(fieldName);
-    renderNotes();
-}
-
-function updateFilter() {
-    var checked = $('#show-finished').prop('checked');
-    notesService.setFilterState(checked);
-    renderNotes();
-}
-
-function newNote() {
-    var note = notesService.getNewNote();
-    showDetailPage(note);
-}
-
-function editNote(event) {
-    var id = event.target.value;
-    var note = notesService.getNoteById(id);
-    showDetailPage(note);
-}
-
-function showDetailPage(note) {
-    updateFields(note);
-    clearValidation();
-    $(".overview-page").hide();
-    $(".detail-page").show();
-}
-
-function updateFields(note) {
-    $("#id-field").val(note.id);
-    $("#title-field").val(note.title);
-    $("#description-field").val(note.description);
-    $("#dueDate-field").val(note.dueDate);
-    $("#priority" + note.priority).prop("checked", true);
-    $("#finishedDate-field").val(note.finishedDate);
-    $("#createDate-field").val(note.createDate);
-    updatePriorityView();
-}
-
-function clearValidation() {
-    $("#editForm [name][required]").css("background-color", "");
-    $("#errorMessage").hide();
-}
-
-function validateForm() {
-    clearValidation();
-    var notValid = $("#editForm [name][required]").filter((pos, input) => !input.value);
-    notValid.css("background-color", "red");
-    var valid = notValid.length == 0;
-    if (!valid) {
-        $("#errorMessage").show();
+    function changeStyle(event) {
+        $("#stylesheetLink").attr("href", event.target.value);
     }
-    return valid;
-}
 
-function showOverviewPage() {
-    $(".overview-page").show();
-    $(".detail-page").hide();
-}
+    function updateOrder() {
+        var fieldName = $(".filter-item:checked").prop('id').substr('order-by-'.length);
+        namespace.notesService.orderByField(fieldName);
+        renderNotes();
+    }
 
-function cancelEdit() {
-    showOverviewPage();
-    return false;
-}
+    function updateFilter() {
+        var checked = $('#show-finished').prop('checked');
+        namespace.notesService.setFilterState(checked);
+        renderNotes();
+    }
 
-function saveEdit() {
-    if (!validateForm()) {
+    function newNote() {
+        var note = namespace.notesService.getNewNote();
+        showDetailPage(note);
+    }
+
+    function editNote(event) {
+        var id = event.target.value;
+        var note = namespace.notesService.getNoteById(id);
+        showDetailPage(note);
+    }
+
+    function showDetailPage(note) {
+        updateFields(note);
+        clearValidation();
+        $(".overview-page").hide();
+        $(".detail-page").show();
+    }
+
+    function updateFields(note) {
+        $("#id-field").val(note.id);
+        $("#title-field").val(note.title);
+        $("#description-field").val(note.description);
+        $("#dueDate-field").val(note.dueDate);
+        $("#priority" + note.priority).prop("checked", true);
+        $("#finishedDate-field").val(note.finishedDate);
+        $("#createDate-field").val(note.createDate);
+        updatePriorityView();
+    }
+
+    function clearValidation() {
+        $("#editForm [name][required]").css("background-color", "");
+        $("#errorMessage").hide();
+    }
+
+    function validateForm() {
+        clearValidation();
+        var notValid = $("#editForm [name][required]").filter((pos, input) => !input.value);
+        notValid.css("background-color", "red");
+        var valid = notValid.length == 0;
+        if (!valid) {
+            $("#errorMessage").show();
+        }
+        return valid;
+    }
+
+    function showOverviewPage() {
+        $(".overview-page").show();
+        $(".detail-page").hide();
+    }
+
+    function cancelEdit() {
+        showOverviewPage();
         return false;
     }
-    var simpleNote = $("#editForm").serializeArray().reduce(function (a, x) {
-        a[x.name] = x.value;
-        return a;
-    }, {});
-    notesService.updateNote(note.createNote(simpleNote));
-    showOverviewPage();
-    renderNotes();
-    return false;
-}
 
-function clearFinishedDate() {
-    $("#finishedDate-field").val("");
-    return false;
-}
+    function saveEdit() {
+        if (!validateForm()) {
+            return false;
+        }
+        var simpleNote = $("#editForm").serializeArray().reduce(function (a, x) {
+            a[x.name] = x.value;
+            return a;
+        }, {});
+        namespace.notesService.updateNote(namespace.note.createNote(simpleNote));
+        showOverviewPage();
+        renderNotes();
+        return false;
+    }
 
-function setFinishedDate() {
-    $("#finishedDate-field").val(formatedDate(new Date()));
-    return false;
-}
+    function clearFinishedDate() {
+        $("#finishedDate-field").val("");
+        return false;
+    }
 
-function formatedDate(date) {
-    var day = ("0" + date.getDate()).slice(-2);
-    var month = ("0" + (date.getMonth() + 1)).slice(-2);
-    var year = date.getFullYear();
-    return year + "-" + month + "-" + day;
-}
+    function setFinishedDate() {
+        $("#finishedDate-field").val(formatedDate(new Date()));
+        return false;
+    }
 
-function renderNotes() {
-    var notesTemplateText = $("#notes-template").html();
-    $(".note-bar").get(0).innerHTML = Handlebars.compile(notesTemplateText)(notesService.getVisibleNotesOrdered());
-    $(".edit-button").click(editNote);
-}
+    function formatedDate(date) {
+        var day = ("0" + date.getDate()).slice(-2);
+        var month = ("0" + (date.getMonth() + 1)).slice(-2);
+        var year = date.getFullYear();
+        return year + "-" + month + "-" + day;
+    }
 
-function updatePriorityView() {
-    var pos = parseInt($("input[type='radio'].priorityInput:checked").attr("value"));
-    $(".priority-field > label:nth-child(-n+" + pos + ") > img").removeClass("grayscale");
-    $(".priority-field > label:nth-last-child(-n+" + (5 - pos) + ") > img").addClass("grayscale");
-}
+    function renderNotes() {
+        var notesTemplateText = $("#notes-template").html();
+        $(".note-bar").get(0).innerHTML = Handlebars.compile(notesTemplateText)(namespace.notesService.getVisibleNotesOrdered());
+        $(".edit-button").click(editNote);
+    }
 
-function registerHandlebarIfLte() {
-    Handlebars.registerHelper('ifLte', function (v1, v2, options) {
-        return (v1 <= v2) ? options.fn(this) : options.inverse(this);
-    });
-}
+    function updatePriorityView() {
+        var pos = parseInt($("input[type='radio'].priorityInput:checked").attr("value"));
+        $(".priority-field > label:nth-child(-n+" + pos + ") > img").removeClass("grayscale");
+        $(".priority-field > label:nth-last-child(-n+" + (5 - pos) + ") > img").addClass("grayscale");
+    }
+
+    function registerHandlebarIfLte() {
+        Handlebars.registerHelper('ifLte', function (v1, v2, options) {
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        });
+    }
+}) (window.notesAppNamespace = window.notesAppNamespace || {});
