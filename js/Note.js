177a50de1230dbd9d67a89892645b22c9c118b42
David Leuenberger <david.leuenberger@hsr.ch>
2016-10-12 18:28:19 +0200

    use moment.js to create a new date, add formating methode

--- js/Note.js
+++ js/Note.js
@@ -10,7 +10,7 @@
             constructor(simpleNote) {
                 if (typeof simpleNote === 'number') {        // create new Note
                     this.id = simpleNote;
-                    this.createDate = privateFormatedDate(new Date());
+                    this.createDate = createNewDate();
                     this.priority = 1;
                 } else if (typeof simpleNote === 'object') {   // create Note from simple Object
                     this.id = simpleNote.id;
@@ -24,19 +24,16 @@
                     throw "InvalidArgumentException " + simpleNote;
                 }
             }
-            get formattedDueDate() {
-                return moment(this.dueDate, ["YYYY-MM-DD", "DD.MM.YYYY"]).fromNow();
-            };
-            get formattedFinishedDate() {
-                return moment(this.finishedDate, ["YYYY-MM-DD", "DD.MM.YYYY"]).fromNow();
-            };
+            get formattedDueDate() { return formatDateFromNow(this.dueDate); };
+            get formattedFinishedDate() { return formatDateFromNow(this.finishedDate); };
         }
 
-        function privateFormatedDate(date) {
-            var day = ("0" + date.getDate()).slice(-2);
-            var month = ("0" + (date.getMonth() + 1)).slice(-2);
-            var year = date.getFullYear();
-            return year + "-" + month + "-" + day;
+        function formatDateFromNow(date) {
+            return moment(date, ["YYYY-MM-DD", "DD.MM.YYYY"]).fromNow();
+        }
+
+        function createNewDate() {
+            return moment().format('YYYY-MM-DD');
         }
 
         function publicCreateNote(simpleNote) {
