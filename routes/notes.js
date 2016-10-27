7b03a11462b8f0bb0f93e05e79224ff0aa37c69d
David Leuenberger <david.leuenberger@hsr.ch>
2016-10-27 07:24:39 +0200

    add updateNote url

--- routes/notes.js
+++ routes/notes.js
@@ -4,7 +4,8 @@ var notes = require('../controller/notesController.js');
 
 
 router.get("/", notes.allNotes);
-router.post("/", notes.addNote);
 router.get("/:id/", notes.getNote);
+router.post("/", notes.addNote);
+router.post("/:id/", notes.updateNote);
 
 module.exports = router;
